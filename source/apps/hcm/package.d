16e98e25324d28ddbe6754f041b33ebbfd38ab61
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-17 11:32:16 +0200

    Update

--- source/apps/hcm/package.d
+++ source/apps/hcm/package.d
@@ -1,1 +1,34 @@
 module apps.hcm;
+
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public import uim.core;
+public import uim.bootstrap;
+public import uim.html;
+public import uim.oop;
+public import uim.models;
+public import uim.apps;
+public import uim.controls;
+public import uim.servers;
+
+public import langs.javascript;
+
+public {
+  import apps.hcm.controllers;
+  import apps.hcm.helpers;
+  import apps.hcm.tests;
+  import apps.hcm.views;
+}
+
+DApp hcmApp;
+static this() {
+  hcmApp = App
+    .name("hcmApp")
+    .rootPath("/apps/hcm")
+    .addRoute(Route("", HTTPMethod.GET, HCMIndexPageController));
+    .addRoute(Route("/", HTTPMethod.GET, HCMIndexPageController));
+}
